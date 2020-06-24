4776991595e3450abbc9349aac7eb61947e91ee8
Mr Robot <davidallandede@hotmail.com>
2020-06-24 15:23:25 -0300

    Adicionando a aula 2

---
+++ Aula-02/FN.Store/FN.Store.UI/Controllers/ProdutosController.cs
@@ -0,0 +1,44 @@
+﻿using FN.Store.UI.Data;
+using FN.Store.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FN.Store.UI.Controllers
+{
+    public class ProdutosController:Controller
+    {
+        public ViewResult Index()
+        {
+
+            IList<Produto> produtos = null;
+            using (var ctx = new FNStoreDataContext())
+            {
+                produtos = ctx.Produtos.ToList();
+            }
+
+            return View(produtos);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Add(Produto produto)
+        {
+
+            //todo add na tabela
+            return View();
+        }
+
+
+
+
+    }
+}
