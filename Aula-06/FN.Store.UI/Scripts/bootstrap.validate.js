5b8b6ddaa4b32c0b2fee9a7461aeab4969bb7b16
Mr Robot <davidallandede@hotmail.com>
2020-06-26 20:56:08 -0300

    Realizado a validacao do formulario com jquery.validate

---
+++ Aula-06/FN.Store.UI/Scripts/bootstrap.validate.js
@@ -0,0 +1,20 @@
+﻿$.validator.setDefaults({
+
+    //QUANDO FOR ACIONADO  < QUANDO O ELEMENTO FOR INVALIDO
+    highlight: function (element) {
+
+        $(element)
+            .closest('.form-group') //DEFININDO QUE VAI PEGAR TODOS OS ELEMENTOS DO FORM GROUP
+            .find('input,label,select,textarea,span') //PEGAR QUAIS ELEMENTOS DETRO DO FORMGROUP QUE VAI MUDAR
+            .addClass('is-invalid')//ADICIONANDO A CLASSE DENTRO DESSES ELEMENTOS DO FIND
+    },
+
+    //QUANDO FOR ACIONADO < QUANDO O ELEMENTO É VALIDO
+    unhighlight: function (element) {
+        $(element)
+            .closest('.form-group')
+            .find('input,label,select,textarea,span')
+            .removeClass('is-invalid')
+    },
+
+})
