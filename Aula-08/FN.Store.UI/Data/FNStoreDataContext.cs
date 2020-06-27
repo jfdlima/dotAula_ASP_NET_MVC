764d884ee6978ae2c03858b284911286c61d5da9
Mr Robot <davidallandede@hotmail.com>
2020-06-27 11:33:36 -0300

    Modificando a arquitetura do projeto

---
+++ Aula-08/FN.Store.UI/Data/FNStoreDataContext.cs
@@ -0,0 +1,18 @@
+﻿using FN.Store.UI.Models;
+using System.Data.Entity;
+
+namespace FN.Store.UI.Data
+{
+    public class FNStoreDataContext:DbContext
+    {
+        public FNStoreDataContext():base("StoreConn")
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<TipoDeProduto> TipoDeProdutos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
